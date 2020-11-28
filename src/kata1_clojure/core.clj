54e692585ce3b0eb865df3f762d3b990d7fa844e
Carlos Roque <carlosroque198@gmail.com>
2020-11-28 15:11:03 -0400

    First two tests passed. Can convert letters and words to plain text

--- src/kata1_clojure/core.clj
+++ src/kata1_clojure/core.clj
@@ -1,6 +1,20 @@
 (ns kata1-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def morse
+  {
+    ".----" "1", "..---" "2", "...--" "3", "....-" "4", "....." "5",
+    ".-" "A", "-..." "B", "-.-." "C", "-.." "D", "." "E", "..-." "F",
+    "--." "G", "...." "H", ".." "I", ".---" "J", "-.-" "K", ".-.." "L",
+    "--" "M", "-." "N", "---" "O", ".--." "P", "--.-" "Q", ".-." "R",
+    "..." "S", "-" "T", "..-" "U", "...-" "V", ".--" "W", "-..-" "X",
+    "-.--" "Y", "--.." "Z"
+  })
+
+(defn morse-to-text [text]
+  (clojure.string/join (into [] (morse-code text))))
+
+(defn morse-code [morse-word]
+  (for [letter (clojure.string/split morse-word #" ")]
+    (get morse letter)))
+
+(morse-to-text ". .-..")
--- test/kata1_clojure/core_test.clj
+++ test/kata1_clojure/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [kata1-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest morse-to-text-test
+  (testing "morse letter to plain text"
+    (is (= "E" (morse-to-text "."))))
+  (testing "morse word to plain text"
+    (is (= "HELLO" (morse-to-text ".... . .-.. .-.. ---"))))
+)
+
+(run-tests)
