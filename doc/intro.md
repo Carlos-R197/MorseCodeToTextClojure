60246f7a15d632fe3f5837091117776fff9aa03d
Carlos Roque <carlosroque198@gmail.com>
2020-11-28 14:55:43 -0400

    Project created

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kata1_clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kata1_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns kata1-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [kata1-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
